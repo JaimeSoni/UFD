8eb05ea9e3fa63d3aebb761de74972c7880eff17
JoannaQuijano12 <garciavane1209@gmail.com>
2025-03-07 17:23:16 -0600

    Versionamiento 18

--- src/UsuarioFinal/uf_inicio.jsx
+++ src/UsuarioFinal/uf_inicio.jsx
@@ -175,17 +175,20 @@ const UFInicio = () => {
                       <p className='titulos-resultados text-xl'>Links: <br /> <span className='textos-resultados'>{publicacion.urls}</span></p>
                     </div>
                     <div className='calificar p-2 w[20%]'>
-                      <p className='calificar-resultados text-xl text-baseazul'>Calificar:</p>
-                      <div className="like-button">
-                        <input className="on" id={`heart-${publicacion.id}`} type="checkbox" />
-                        <label className="like" htmlFor={`heart-${publicacion.id}`}>
-                          <svg className="like-icon" fillRule="nonzero" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
-                          </svg>
-                          <span className="like-text">Likes</span>
-                        </label>
-                      </div>
+                    <p className='calificar-resultados text-xl text-baseazul'>Calificar:</p>
+                    <div className="like-button">
+                      <input className="on" id="heart" type="checkbox" />
+                      <label className="like" htmlFor="heart">
+                        <svg className="like-icon" fillRule="nonzero" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                          <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+                        </svg>
+                        <span className="like-text">Likes</span>
+                      </label>
+
                     </div>
+
+                  </div>
+
                   </div>
                 </div>
               </div>
