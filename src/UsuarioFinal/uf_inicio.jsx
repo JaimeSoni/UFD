449f840ef0c73b2e085767a8f63271cc0ed80b03
JoannaQuijano12 <garciavane1209@gmail.com>
2025-03-07 16:33:27 -0600

    Versionamiento 16

--- src/UsuarioFinal/uf_inicio.jsx
+++ src/UsuarioFinal/uf_inicio.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import '../StylesUsuarioFinal/uf_inicio.css'
 
 import { BiChevronDownCircle } from "react-icons/bi";
+import { FaCreativeCommonsShare } from "react-icons/fa";
 
 
 // Simulación de datos de publicaciones con `id` único
 const publicaciones = [
+
   { id: "pub1", fecha: "21/02/2025", categoria: "Colegiaturas", tema: "Mensualidad sobre los semestres para la preparatoria.", descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos quos, atque asperiores consequatur, sint architecto odio beatae dolores possimus enim, sapiente quaerat? Quae beatae veritatis exercitationem iste eligendi fuga velit!", palabrasClave: "pago, mensualidad, costo", documentos: "", urls: "" },
   { id: "pub2", fecha: "15/02/2025", categoria: "Trámites", tema: "Proceso para solicitar constancia de estudios", descripcion: "Información sobre el proceso para solicitar constancias oficiales y los documentos necesarios.", palabrasClave: "constancia, documentos, trámite", documentos: "", urls: "" },
   { id: "pub3", fecha: "10/02/2025", categoria: "Becas", tema: "Convocatoria para becas académicas", descripcion: "Detalles sobre las becas disponibles para estudiantes de alto rendimiento.", palabrasClave: "beca, apoyo, financiamiento", documentos: "", urls: "" }
@@ -14,10 +16,10 @@ const publicaciones = [
 const UFInicio = () => {
   const [expandedId, setExpandedId] = useState(null);
   const [hasSearched, setHasSearched] = useState(false);
-      
+
   // Función de filtro de búsqueda
   const [filtroPublicacion, setfiltroPublicacion] = useState('');
-  
+
   const handleInputChange = (e) => {
     setfiltroPublicacion(e.target.value);
   };
@@ -37,9 +39,9 @@ const UFInicio = () => {
   // Función para filtrar publicaciones
   const filtrarPublicaciones = () => {
     if (!filtroPublicacion.trim()) return [];
-    
+
     const terminoBusqueda = filtroPublicacion.toLowerCase().trim();
-    
+
     return publicaciones.filter(publicacion => {
       return (
         publicacion.categoria.toLowerCase().includes(terminoBusqueda) ||
@@ -83,14 +85,14 @@ const UFInicio = () => {
         <div className='buscador flex items-center justify-center'>
           <div className="search-panels-filtro">
             <div className="search-group">
-              <input 
-                required 
-                type="text" 
-                name="text" 
-                autoComplete="on" 
-                className="input" 
-                value={filtroPublicacion} 
-                onChange={handleInputChange} 
+              <input
+                required
+                type="text"
+                name="text"
+                autoComplete="on"
+                className="input"
+                value={filtroPublicacion}
+                onChange={handleInputChange}
                 onKeyPress={handleKeyPress}
               />
               <label className="enter-label">Realizar Busqueda . . .</label>
@@ -133,68 +135,75 @@ const UFInicio = () => {
       {/* Resultador */}
       <div className='flex items-center justify-center'>
         <div className='resultados-final w-[95%] h-[50%] mt-3 overflow-y-auto'>
-          {hasSearched ? (
-            resultadosFiltrados.length > 0 ? (
-              // Mostrar resultados filtrados
-              resultadosFiltrados.map((publicacion) => (
-                <div key={publicacion.id} className={`articulos-finales p-1 flex flex-col transition-all duration-300 ${expandedId === publicacion.id ? "expanded" : ""}`}>
-                  {/* Contenedor principal */}
-                  <div className="flex h-[50px] items-center justify-center">
-                    <div className='fecha-final w-[15%] flex items-center justify-center text-xl'>
-                      <h1>{publicacion.fecha}</h1>
-                    </div>
+          {publicaciones.map((publicacion) => (
+            <div key={publicacion.id} className={`articulos-finales p-1 flex flex-col transition-all duration-300 ${expandedId === publicacion.id ? "expanded" : ""}`}>
 
-                    <div className='categoria-final w-[15%] flex items-center justify-center text-xl'>
-                      <h1 className='bg-basenaranja p-2 rounded-[15px] text-baseblanco'>{publicacion.categoria}</h1>
-                    </div>
+              {/* Contenedor principal */}
+              <div className="flex h-[50px] items-center justify-center">
+                <div className='fecha-final w-[15%] flex items-center justify-center text-xl'>
+                  <h1>{publicacion.fecha}</h1>
+                </div>
 
-                    <div className='tema-final w-[50%] flex items-center justify-center text-xl'>
-                      <h1>{publicacion.tema}</h1>
-                    </div>
+                <div className='categoria-final w-[15%] flex items-center justify-center text-xl'>
+                  <h1 className='bg-basenaranja p-2 rounded-[15px] text-baseblanco'>{publicacion.categoria}</h1>
+                </div>
 
-                    <div className='expandir-contraer w-[10%] flex items-center justify-center text-4xl'>
-                      <button className='icono-final' onClick={() => toggleExpand(publicacion.id)} >
-                        <BiChevronDownCircle className='text-basenaranja' />
-                      </button>
-                    </div>
+                <div className='tema-final w-[50%] flex items-center justify-center text-xl'>
+                  <h1>{publicacion.tema}</h1>
+                </div>
+
+                <div className='expandir-contraer w-[10%] flex items-center justify-center text-4xl'>
+                  <button className='icono-final' onClick={() => toggleExpand(publicacion.id)} >
+                    <BiChevronDownCircle className='text-basenaranja' />
+                  </button>
+                </div>
+
+                <div className='copiar-articulo w-[10%] flex items-center justify-center text-4xl'>
+                  <button >
+                    <FaCreativeCommonsShare className='text-baseazul' title='Copiar Enlace' />
+                  </button>
+                </div>
+              </div>
+
+              <div
+                className={`transition-all duration-300 overflow-hidden ${expandedId === publicacion.id ? "max-h-[300px]" : "max-h-0"
+                  }`}
+              >
+                <div className="descripcion p-2 h-[120px]">
+                  <p className='titulos-resultados text-xl'>Descripcion: <br /> <span className='textos-resultados'>{publicacion.descripcion}</span></p>
+                </div>
+                <div className="palabras-clave p-2 h-[100px]">
+                  <p className='titulos-resultados text-xl'>Palabras Clave: <br /> <span className='text-baseblanco text-[15px] bg-baseazul p-2 rounded-[20px]'>{publicacion.palabrasClave}</span></p>
+                </div>
+                <div className='flex'>
+                  <div className="documentos p-2 w-[40%] h-[80px]">
+                    <p className='titulos-resultados text-xl'>Documentos: <br /> <span className='textos-resultados'> {publicacion.documentos}</span></p>
                   </div>
+                  <div className="urls p-2 w-[40%] h-[80px]">
+                    <p className='titulos-resultados text-xl'>Links: <br /> <span className='textos-resultados'>{publicacion.urls}</span></p>
+                  </div>
+                  <div className='calificar p-2 w[20%]'>
+                    <p className='calificar-resultados text-xl text-baseazul'>Calificar:</p>
+                    <div className="like-button">
+                      <input className="on" id="heart" type="checkbox" />
+                      <label className="like" htmlFor="heart">
+                        <svg className="like-icon" fillRule="nonzero" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                          <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+                        </svg>
+                        <span className="like-text">Likes</span>
+                      </label>
 
-                  <div
-                    className={`transition-all duration-300 overflow-hidden ${expandedId === publicacion.id ? "max-h-[300px]" : "max-h-0"
-                      }`}
-                  >
-                    <div className="descripcion p-2 h-[120px]">
-                      <p className='titulos-resultados text-xl'>Descripcion: <br /> <span className='textos-resultados'>{publicacion.descripcion}</span></p>
-                    </div>
-                    <div className="palabras-clave p-2 h-[100px]">
-                      <p className='titulos-resultados text-xl'>Palabras Clave: <br /> <span className='text-baseblanco text-[15px] bg-baseazul p-2 rounded-[20px]'>{publicacion.palabrasClave}</span></p>
                     </div>
 
-                    <div className='flex'>
-                      <div className="documentos p-2 w-[50%] h-[80px]">
-                        <p className='titulos-resultados text-xl'>Documentos: <br /> <span className='textos-resultados'> {publicacion.documentos}</span></p>
-                      </div>
-                      <div className="urls p-2 w-[50%] h-[80px]">
-                        <p className='titulos-resultados text-xl'>Links: <br /> <span className='textos-resultados'>{publicacion.urls}</span></p>
-                      </div>
-                    </div>
                   </div>
                 </div>
-              ))
-            ) : (
-              <div className="text-center text-coloralternodos text-xl pt-20">
-                No se encontraron resultados para "{filtroPublicacion}"
               </div>
-            )
-          ) : (
-            <div className="text-center text-coloralternodos text-xl pt-20">
-              Puedes realizar búsqueda mediante algún tema, categoría o alguna palabra clave . . . 
             </div>
-          )}
+          ))}
         </div>
       </div>
 
-    </div>
+    </div >
   )
 }
 
